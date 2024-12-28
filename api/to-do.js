03c1f19734562ae7ec6d4fffe0e629870d73bc17
Unnati Matai (Devops <GV0734@ZKGI.COM>
2024-12-29 02:12:48 +0530

    changed

--- api/to-do.js
+++ api/to-do.js
@@ -7,14 +7,14 @@ app.use(bodyParser.json());
 // Define your routes
 app.get('/v1/to-do', (req, res) => {
     // Get the IP address from the 'X-Forwarded-For' header
-    const ipAddress = req.connection.remoteAddress;
-    const ipAddressGiven = req.headers?.['x-forwarded-for'];
+    const ipAddressFromHeader = req.headers['x-client-ip'] || "IP not set";
+    const ipAddressGiven = req.headers['x-forwarded-for'] || "Nothing given";
   
     // Send the response with the IP address in the header
     res.status(200).json({
       message: 'To-do items listed here',
-      ipAddressGiven: ipAddressGiven || "Nothing given",
-      clientIpCaptured: ipAddress,
+      ipAddressFromHeader: ipAddressFromHeader,
+      ipAddressGiven: ipAddressGiven,
     });
   });
 
