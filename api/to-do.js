eee959e9c1b1421e9ec9a629292e2755debd5b45
Unnati Matai (Devops <GV0734@ZKGI.COM>
2024-12-29 01:47:47 +0530

    Client IP added

--- api/to-do.js
+++ api/to-do.js
@@ -6,8 +6,15 @@ app.use(bodyParser.json());
 
 // Define your routes
 app.get('/v1/to-do', (req, res) => {
-  res.status(200).json({ message: 'To-do items listed here' });
-});
+    // Get the IP address from the 'X-Forwarded-For' header
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+    // Send the response with the IP address in the header
+    res.status(200).json({
+      message: 'To-do items listed here',
+      clientIp: ipAddress
+    });
+  });
 
 // Export your express app as a serverless functionn
 module.exports = (req, res) => {
